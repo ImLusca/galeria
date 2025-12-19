b43b8a2932ffb63516dfa4091a2c147591f403ee
lusca <lucaspacheco747@gmail.com>
2025-12-19 00:35:41 -0300

    quickFix

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/galeria",
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
+  build: {
+    outDir: "docs",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
